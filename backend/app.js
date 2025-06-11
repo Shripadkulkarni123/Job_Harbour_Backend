97aa24fb13debdfe9e45eaef02517d9b9bc0cba2
Shripadkulkarni123 <150930052+Shripadkulkarni123@users.noreply.github.com>
2025-06-11 23:49:02 +0530

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -27,11 +27,18 @@ app.use(express.urlencoded({ extended: true }));
 
 // Add security headers
 app.use((req, res, next) => {
+  const allowedOrigins = ["https://job-harbour-01.netlify.app", "http://localhost:5173"];
+  const origin = req.headers.origin;
+  
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  
   res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
   res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
-  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
